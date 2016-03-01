513a0d4b31700fff9123fee6dc3bb7b511451d7e
KristiinaK6iv <kristiina.k6iv@gmail.com>
2016-03-01 21:22:56 +0200

    Uus avalehe kujundus

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,89 +1,83 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+    <meta charset="utf-8"/>
+	<title>P2P kuulutused</title>
+	<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" />
 </head>
+
+
 <body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	
+	<div class="jumbotron">
+		<div class="container">
+			<h1>P2P kuulutused</h1>
+		</div>
+		
 	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
+	
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+		
+		<!--Logo-->
+		
+		
+		<!--Menu Items-->
+		<div>
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="index.php">Avaleht</a></li>
+				<li><a href="index.php/welcome/kuulutused">Kuulutused</a></li>
+				<li><a href="index.php/welcome/minuKuulutused">Minu kuulutused</a></li>
+				<li><a href="index.php/welcome/KKK">KKK</a></li>
+				<li><a href="index.php/welcome/kontakt">Kontakt</a></li>
+			</ul>
+		</div>
+		</div>
+	</nav>
+	
+	<div class="container">
+		<section>
+			<div class="page-header" id="avaleht">
+				<h2> Kuulutused </h2>
+			</div>
+			
+			<div class="row">
+				<div class="col-lg-4">
+					<blockquote>
+						<ul>
+							<li><a href="#">IT-teenused</a></li>
+							<li><a href="#">Finantsteenused</a></li>
+						</ul>
+						
+					</blockquote>
+				</div>
+				<div class="col-lg-4">
+					<blockquote>
+						<ul>
+							<li><a href="#">Õpetamine</a></li>
+							<li><a href="#">Iluteenused</a></li>
+						</ul>
+					</blockquote>
+				</div>
+				<div class="col-lg-4">
+					<blockquote>
+						<ul>
+							<li><a href="#">Puhastusteenused</a></li>
+							<li><a href="#">Varia</a></li>
+						</ul>
+					</blockquote>
+				</div>
+			</div>
+		</section>
+		
+	</div>
+	
+	<div class="container text-center">
+		<hr>
+		<p>&copy; Copyright @ 2016</p>
+	</div>
+	
+	
 </body>
 </html>
