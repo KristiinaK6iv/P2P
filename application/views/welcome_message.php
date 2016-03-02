ff0934b30872195cf0424b35f5f003905642c180
heleandra <kuulmets@gmail.com>
2016-03-02 10:28:44 +0200

    welcome

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,41 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8"/>
-	<title>P2P kuulutused</title>
-	<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" />
-</head>
-
-
-<body>
-	
-	<div class="jumbotron">
-		<div class="container">
-			<h1>P2P kuulutused</h1>
-		</div>
-		
-	</div>
-	
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-		
-		<!--Logo-->
-		
-		
-		<!--Menu Items-->
-		<div>
-			<ul class="nav navbar-nav">
-				<li class="active"><a href="index.php">Avaleht</a></li>
-				<li><a href="index.php/welcome/kuulutused">Kuulutused</a></li>
-				<li><a href="index.php/welcome/minuKuulutused">Minu kuulutused</a></li>
-				<li><a href="index.php/welcome/KKK">KKK</a></li>
-				<li><a href="index.php/welcome/kontakt">Kontakt</a></li>
-			</ul>
-		</div>
-		</div>
-	</nav>
-	
 	<div class="container">
 		<section>
 			<div class="page-header" id="avaleht">
@@ -72,12 +34,3 @@
 		</section>
 		
 	</div>
-	
-	<div class="container text-center">
-		<hr>
-		<p>&copy; Copyright @ 2016</p>
-	</div>
-	
-	
-</body>
-</html>
