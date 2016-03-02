90bba0fbffabcb4969272cc9f58e6764c4a9e17c
heleandra <kuulmets@gmail.com>
2016-03-02 10:15:12 +0200

    yla- ja alaosa

---
+++ application/views/bar/alaosa.php
@@ -0,0 +1,4 @@
+<div class="container text-center">
+    <hr>
+    <p>&copy; Copyright @ 2016</p>
+</div>
---
+++ application/views/bar/ylaosa.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8"/>
+    <title>P2P kuulutused</title>
+    <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+</head>
+
+
+<body>
+
+<div class="jumbotron">
+    <div class="container">
+        <h1>P2P kuulutused</h1>
+    </div>
+
+</div>
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+
+        <!--Logo-->
+
+
+        <!--Menu Items-->
+        <div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php">Avaleht</a></li>
+                <li><a href="index.php/welcome/kuulutused">Kuulutused</a></li>
+                <li><a href="index.php/welcome/minuKuulutused">Minu kuulutused</a></li>
+                <li><a href="index.php/welcome/KKK">KKK</a></li>
+                <li><a href="index.php/welcome/kontakt">Kontakt</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
