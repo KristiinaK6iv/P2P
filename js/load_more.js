d322a96f19124e94ba2805eaf60e031e8706aa37
heleandra <kuulmets@gmail.com>
2016-03-05 14:06:00 +0200

    html valideeruvus

--- js/load_more.js
+++ js/load_more.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
     size_li = $("#KKK li").size();
-    x=10;
+    x=5;
     $('#KKK li:lt('+x+')').show();
+    $('#KKK h4:lt('+x+')').show();
     $('#loadMore').click(function () {
         x= (x+10 <= size_li) ? x+10 : size_li;
         $('#KKK li:lt('+x+')').show();
+        $('#KKK h4:lt('+x+')').show();
         $('#loadMore').hide();
     });
 });
