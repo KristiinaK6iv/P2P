81b9c8a49457e141f25f180e02b29874a39a1755
KristiinaK6iv <kristiina.k6iv@gmail.com>
2016-03-06 02:02:41 +0200

    Removing WCAG error

--- application/views/kontakt.php
+++ application/views/kontakt.php
@@ -5,7 +5,7 @@ include('ylaosa.php')
 		<div class="row">
 			<div id="logo" class="col-md-4 col-md-offset-1"><img src="<?php echo base_url(); ?>images/logo.png" class="img-responsive" alt="logo"></div>
 			<div class="col-md-6">
-				<h3 class="text-center">Kontakt</h3><br>
+				<h3>Kontakt</h3><br>
 				<p class="text-center">Küsimuste või probleemide korral palume ühendust võtta e-maili või telefoni teel.</p>
 				<p class="text-center">p2pkuulutused@p2p.ee</p>
 				<p class="text-center">+372 5858585</p>
