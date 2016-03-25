3bfa547b5da5b8a3da5b0346bc9ffebaa1194c9a
heleandra <kuulmets@gmail.com>
2016-03-25 15:28:38 +0200

    removed console.log

--- js/address_listener.js
+++ js/address_listener.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $.address.change(function(event) {
-        console.log("muutus");
-        console.log("laeb tabeli " + event.value.split("/")[1]);
-        load_data_ajax(event.value.split("/")[1], "http://localhost:8080/index.php");
+        if(event.value.split("/")[1] != "") {
+            load_data_ajax(event.value.split("/")[1], "http://localhost:8080/index.php");
+        }
     });
 
     $(".categoryButton").click(function() {
