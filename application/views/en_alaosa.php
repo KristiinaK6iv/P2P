3c45a52dc44a059becb35721fb47811756267b88
heleandra <kuulmets@gmail.com>
2016-04-08 17:50:14 +0300

    css

--- application/views/en_alaosa.php
+++ application/views/en_alaosa.php
@@ -1,7 +1,7 @@
 
 <div class="container text-center">
     <hr>
-    <p  class="footer">Total ads: <span id="total"></span></p>
+    <p  class="footer text-left">Total ads: <span id="total"></span></p>
     <a  class="footer" href="<?php echo base_url(); ?>index.php/welcome/enSisukaart">Sitemap</a>
     <p  class="footer">&copy; Copyright 2016</p>
 </div>
