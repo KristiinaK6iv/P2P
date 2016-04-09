bc7146fb5a36a4192de5bc0c3404b4569c7f0bcb
KristiinaK6iv <kristiina.k6iv@gmail.com>
2016-04-09 16:35:23 +0300

    XML põhiste keelte kooskasutus

--- application/views/en_kontakt.php
+++ application/views/en_kontakt.php
@@ -7,6 +7,24 @@
 				<p class="text-center">If you have any questions or problems then contact us by phone or e-mail.</p>
 				<p class="text-center">p2pkuulutused@p2p.ee</p>
 				<p class="text-center">+372 5858585</p>
+				<div class="alert alert-info text-center">
+				<strong>Address:</strong> <br><br>
+				<!--xml ja namespaces-->
+				<?php
+				$feed = new SimpleXMLElement("http://localhost/P2P/xml/file.xml",NULL,TRUE);
+				$namespaces = $feed->getNamespaces(true);
+				
+				$getChildren=$feed->children($namespaces["k"]);
+				echo $getChildren->TÄNAV."<br>";
+				echo $getChildren->LINN."<br>";
+				echo $getChildren->RIIK."<br>";
+				echo $getChildren->POSTIINDEKS."<br>";
+				
+				//Ilma namespace'ita
+				$feed2=simplexml_load_file("http://localhost/P2P/xml/file.xml");
+				echo $feed2->POSTIINDEKS;
+				?>
+				</div>
 			</div>
 		</div>
 	</div>
--- application/views/kontakt.php
+++ application/views/kontakt.php
@@ -7,8 +7,28 @@
 				<p class="text-center">Küsimuste või probleemide korral palume ühendust võtta e-maili või telefoni teel.</p>
 				<p class="text-center">p2pkuulutused@p2p.ee</p>
 				<p class="text-center">+372 5858585</p>
+				<div class="alert alert-info text-center">
+				<strong>Aadress:</strong> <br><br>
+				<!--xml ja namespaces-->
+				<?php
+				$feed = new SimpleXMLElement("http://localhost/P2P/xml/file.xml",NULL,TRUE);
+				$namespaces = $feed->getNamespaces(true);
+				
+				$getChildren=$feed->children($namespaces["k"]);
+				echo $getChildren->TÄNAV."<br>";
+				echo $getChildren->LINN."<br>";
+				echo $getChildren->RIIK."<br>";
+				echo $getChildren->POSTIINDEKS."<br>";
+				
+				//Ilma namespace'ita
+				$feed2=simplexml_load_file("http://localhost/P2P/xml/file.xml");
+				echo $feed2->POSTIINDEKS;
+				?>
+				</div>
 			</div>
 		</div>
+		
+		
 	</div>
 <?php
 include('alaosa.php')
